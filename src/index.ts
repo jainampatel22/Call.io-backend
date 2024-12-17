1a78267ff6b3a9bbc326923ea925c995105a11e9
=jainam patel <pateljainams122@gmail.com>
2024-12-17 17:33:15 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,9 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "*",
-        methods: ["GET", "POST"]
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type"],
+        credentials: true,
     }
 });
 
